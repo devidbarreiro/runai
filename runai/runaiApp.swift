3fb70e30a035302055279e8f76cc7371d685e632
david <david.barreiro@criterial.cc>
2025-09-11 13:42:30 +0200

    Initial Commit

---
+++ runai/runaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  runaiApp.swift
+//  runai
+//
+//  Created by David Barreiro on 11/09/25.
+//
+
+import SwiftUI
+
+@main
+struct runaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ runaiTests/runaiTests.swift
@@ -0,0 +1,17 @@
+//
+//  runaiTests.swift
+//  runaiTests
+//
+//  Created by David Barreiro on 11/09/25.
+//
+
+import Testing
+@testable import runai
+
+struct runaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
